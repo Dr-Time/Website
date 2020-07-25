39a16d69b541e80b66232321446c1bd977be1470
Gaurav Misra <gmisra197@gmail.com>
2020-07-25 04:52:14 -0400

    Updates

---
+++ precache-manifest.a27b706d2afcf12f66a04935727b92c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df3e8341a9f14c16a7c116544b0a7e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98559257b38f3ff7c031",
+    "url": "/static/css/main.c0fdcd69.chunk.css"
+  },
+  {
+    "revision": "5ae28d9a3381b8bf8f63",
+    "url": "/static/js/2.57670ac7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57670ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98559257b38f3ff7c031",
+    "url": "/static/js/main.a228a195.chunk.js"
+  },
+  {
+    "revision": "b58c779bb18b87f56449",
+    "url": "/static/js/runtime-main.eab99e6e.js"
+  },
+  {
+    "revision": "a6d5330275d1853023dd9a261ae1d4f1",
+    "url": "/static/media/logo.a6d53302.jpg"
+  }
+]);
